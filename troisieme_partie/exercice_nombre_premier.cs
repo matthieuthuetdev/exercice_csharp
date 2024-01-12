c230690b166c2f60178dd8766709ea70027434c0
matthieuthuetdev <mthuet.pro@gmail.com>
2024-01-12 11:55:06 +0100

    exercie nombre premier

--- troisieme_partie/exercice_nombre_premier.cs
+++ troisieme_partie/exercice_nombre_premier.cs
@@ -4,9 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Net.Mail;
 
 namespace Demarrage
-        {
-    
-        }
+{
+    internal class Program
+    {
+        private static object lettre;
+
+        static void Main(string[] args)
+        {        }
 
     }
 }
