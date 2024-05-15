3b95538cc8a23af80ffb461c377bf00609d4fa21
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-05-15 15:49:52 +0200

    création du projet pointlibrary et des première méthode

---
+++ objet/point/pointLibrary/Point.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace pointLibrary
+{
+    public class Point
+    {
+        private int abscisse;
+        private int ordonnee;
+
+        public Point(int abscisse, int ordonnee)
+        {
+            this.abscisse = abscisse;
+            this.ordonnee = ordonnee;
+        }
+        public Point()
+        {
+            this.abscisse = 0;
+            this.ordonnee = 0;
+        }
+    }
+}
