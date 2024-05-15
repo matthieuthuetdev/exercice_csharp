9b746df3c44ca2c8bd0069bd922b1ab452c55723
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-05-15 15:54:21 +0200

    implémantation de la classe dans le programme

--- objet/point/app_point/Program.cs
+++ objet/point/app_point/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace app_point
+﻿using System.Drawing;
+
+namespace app_point
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Point premierPoint = new Point(7, 7);
+            Console.WriteLine(premierPoint.ToString());
         }
     }
 }
