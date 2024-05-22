7a0a2e423355540de000dc32dd3f0f31d232b3e6
MatthieuThuet <98526173+matthieuthuetdev@users.noreply.github.com>
2024-05-22 12:52:07 +0200

    Point (#1)
    * création du projet et de la solution point
    
    * déplacement du dossier point dans le dossier objet
    
    * création du projet pointlibrary et des première
    méthode
    
    * implémantation de la classe dans le programme
    
    * push
    
    * métode simetrie
    
    * push
    
    * push
    
    * ajout du name space avant l'apelle des classe
    
    * modifiquation du nom des objet créé
    
    * ajout de la méthode permuter
    
    * ajout des teste
    
    * correction de la plus part des bug
    
    * correction des derrnier bug
    
    * vertion finale
    
    * correction de type

---
+++ objet/point/app_point/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using pointLibrary;
+
+namespace app_point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object ordosim;
+            object abscisim;
+            object origineSim;
+            pointLibrary.Point premierPoint = new pointLibrary.Point(7, 7);
+            Console.WriteLine(premierPoint.ToString());
+            Console.Write("utilisation de la fonction mov avec les paramaitres 4 9");
+            premierPoint.moov(4, 9);
+            Console.WriteLine(premierPoint.ToString());
+            Console.WriteLine("utilisation de la fonction simAbscisse");
+            abscisim = premierPoint.simAbscisse();
+            Console.WriteLine(abscisim.ToString());
+            Console.WriteLine("utilisation de la fonction simOrdonnee");
+            ordosim = premierPoint.simOrdonnee();
+            Console.WriteLine(ordosim.ToString());
+            Console.WriteLine("utilisation de la fonction simorigine");
+            origineSim =  origineSim = premierPoint.simOrigine();
+            Console.WriteLine(origineSim.ToString());
+            Console.WriteLine("utilisation de la fonction permuter");
+            premierPoint.permuter();
+            Console.WriteLine(premierPoint.ToString());
+
+
+        }
+    }
+}
---
+++ objet/point/pointLibrary/Point.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.SqlTypes;
+using System.Formats.Asn1;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace pointLibrary
+{
+    public class Point
+    {
+        private int abscisse;
+        private int ordonnee;
+
+        public Point(int abscisse, int ordonnee)
+        {
+            this.abscisse = abscisse;
+            this.ordonnee = ordonnee;
+        }
+        public Point()
+        {
+            this.abscisse = 0;
+            this.ordonnee = 0;
+        }
+        public void moov(int nouvelleAbscisse, int nouvelleOrdonnee)
+        {
+            this.abscisse = nouvelleAbscisse;
+            this.ordonnee = nouvelleOrdonnee;
+            
+        }
+        public Point simAbscisse()
+        {
+            Point abscisim = new Point(this.abscisse * -1, this.ordonnee);
+            return abscisim;
+        }
+        public Point simOrdonnee()
+        {
+            Point ordosim = new Point(this.abscisse, this.ordonnee * -1);
+            return ordosim;
+        }
+        public Point simOrigine()
+        {
+            Point origineSim = new Point(this.abscisse * -1, this.ordonnee * -1);
+            return origineSim;
+        }
+        public void permuter()
+        {
+            int newOrdonnee = this.abscisse;
+            int newAbscisse = this.ordonnee;
+            this.abscisse = newAbscisse;
+            this.ordonnee = newOrdonnee;
+
+        }
+        public override string ToString()
+        {
+            return "les cordonnée du point sont : X"+ abscisse+"Y"+ordonnee;
+        }
+    }
+}
