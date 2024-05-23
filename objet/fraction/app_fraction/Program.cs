91008e14ef3b1b4f7163d745f8927d16cccab5d1
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-05-23 09:36:14 +0200

    création de la solution et du projet fraction

---
+++ objet/fraction/app_fraction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace app_fraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
