8a05377f3689c1e7a47f7b1f7f06d0b66c724000
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-05-28 16:55:30 +0200

    suppretion des testes

--- objet/fraction/app_fraction/Program.cs
+++ objet/fraction/app_fraction/Program.cs
@@ -7,10 +7,7 @@
             fractionLibrary.Fraction af = new fractionLibrary.Fraction(4, 19);
             Console.WriteLine(af.ToString());
             fractionLibrary.Fraction bf = new fractionLibrary.Fraction(7, 5);
-            Console.WriteLine(bf.ToString());
-            object cf = af.Multiplie(bf);
-            Console.WriteLine(cf.ToString());
-            Console.WriteLine(af);
+            
 
 
         
