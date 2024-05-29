b279d77f0aa34e694a12267227bdf59a12745291
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-05-29 10:12:03 +0200

    création des teste de la fonction multiplie

--- objet/fraction/app_fraction/Program.cs
+++ objet/fraction/app_fraction/Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
-            fractionLibrary.Fraction af = new fractionLibrary.Fraction(4, 19);
+            fractionLibrary.Fraction af = new fractionLibrary.Fraction(2,4);
+            Console.WriteLine("la fraction af est égale à :");
             Console.WriteLine(af.ToString());
-            fractionLibrary.Fraction bf = new fractionLibrary.Fraction(7, 5);
-            
+            fractionLibrary.Fraction bf = new fractionLibrary.Fraction(3,4);
+            Console.WriteLine("la fraction bf est égale à :");
+            Console.WriteLine(bf.ToString());
+            object cf = af.Multiplie(bf);
+            Console.WriteLine("le résulta de la multiplication des deux fraction présédante est de :");
+            Console.WriteLine(cf.ToString());
+            Console.WriteLine("vérification que les deux fraction du début n'ait pas bouger a cause de la fonction multiplier");
+            Console.WriteLine(af.ToString());
+            Console.WriteLine(bf.ToString());
 
 
         
