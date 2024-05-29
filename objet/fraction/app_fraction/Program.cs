5d573d9352f15cea0656a68ab127c86145e92c81
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-05-29 12:49:14 +0200

    création des testes avant validation

--- objet/fraction/app_fraction/Program.cs
+++ objet/fraction/app_fraction/Program.cs
@@ -1,24 +1,31 @@
-﻿namespace app_fraction
+﻿using System.Runtime.InteropServices.ObjectiveC;
+
+namespace app_fraction
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            fractionLibrary.Fraction af = new fractionLibrary.Fraction(2,4);
-            Console.WriteLine("la fraction af est égale à :");
-            Console.WriteLine(af.ToString());
-            fractionLibrary.Fraction bf = new fractionLibrary.Fraction(3,4);
-            Console.WriteLine("la fraction bf est égale à :");
-            Console.WriteLine(bf.ToString());
-            object cf = af.Divise(bf);
-            Console.WriteLine("le résulta de la division deux fraction présédante est de :");
-            Console.WriteLine(cf.ToString());
-            Console.WriteLine("vérification que les deux fraction du début n'ait pas bouger a cause de la fonction multiplier");
-            Console.WriteLine(af.ToString());
-            Console.WriteLine(bf.ToString());
+            fractionLibrary.Fraction af = new fractionLibrary.Fraction(5, 7);
+            fractionLibrary.Fraction bf = new fractionLibrary.Fraction(19, 5);
+            Console.WriteLine("testes finales avant validation.");
+            
+            Console.WriteLine("création de deux fraction, la première : "+af.ToString()+"et la deuxième est "+ bf.ToString());
+            object cf = af.Plus(bf);
+            Console.WriteLine("teste de la fonction Plus, le résulta de la fonction est :"+ cf.ToString());
+            object df = af.Moins(bf);
+            Console.WriteLine("teste de la fonction Moins, le résulta de la fonction est :" + df.ToString());
+            object ef = af.Multiplie(bf);
+            Console.WriteLine("teste de la fonction Multiplie, le résulta de la fonction est :" + ef.ToString());
+            object ff = af.Moins(bf);
+            Console.WriteLine("teste de la fonction divise, le résulta de la fonction est :" + ff.ToString());
+            af.Oppose();
+            Console.WriteLine("teste de la fonction Opose, le résulta de la fonction est :" + af.ToString());
+            af.Inverse();
+            Console.WriteLine("teste de la fonction nverse, le résulta de la fonction est :" + af.ToString());
 
 
-        
+
         }
     }
 }
