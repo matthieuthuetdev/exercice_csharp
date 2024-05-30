0fad79784a830668bf0340f4b000490fd4181035
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-05-30 12:44:42 +0200

    création de la classe checkfraction

---
+++ objet/fraction/fractionLibrary/CheckFraction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractionLibrary
+{
+    internal class CheckFraction : Exception
+    {
+        public CheckFraction()
+        {
+        }
+
+        public CheckFraction(string? Fraction) : base(String.Format("la fraction : {0} est invalide.",Fraction))
+        {
+        }
+    }
+}
